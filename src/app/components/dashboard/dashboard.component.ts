334b7b8152434f3c0887dcb7a070a4a9d8ea17eb
German Alexis <alexisdager93@hotmail.com>
2022-09-23 12:30:33 -0500

    Render view the values the api in pages dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { WeatherService } from '../services/weather.service';
+import { IWeather } from '../../shared/interfaces/weather.interfaces';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,13 +10,25 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  weather$!: Observable<IWeather>;
+
   form = this.fb.group({
     codePostal: [null],
     city: [null],
     codeZona: [null],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private weatherService: WeatherService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('se inicio');
+    this.getWeather();
+  }
+
+  getWeather() {
+    this.weather$ = this.weatherService.getWethearName('monteria');
+  }
 }
--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
+import { IWeather } from '../interfaces/weather.interfaces';
+import { environment } from '../../../environments/environment';
+const baseUrl = environment.openWeather.icon;
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
+  @Input() weather!: IWeather;
+  baseUrlIcon = baseUrl;
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
@@ -17,6 +28,4 @@ export class CardComponent implements OnInit {
   bufferValue = 75;
 
   constructor() {}
-
-  ngOnInit(): void {}
 }
